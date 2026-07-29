e9daafca7b5271122b83891e80f95a7be9439376
EESH-123 <eesha.ghosh@avekshaa.com>
2026-07-29 12:54:59 +0530

    Add autoscaling, branch deployment workflow and gitignore

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -121,3 +121,40 @@ resource "aws_ecs_service" "app" {
     aws_lb_listener.app
   ]
 }
+
+#########################################
+# ECS Auto Scaling Target
+#########################################
+
+resource "aws_appautoscaling_target" "ecs" {
+  max_capacity       = 6
+  min_capacity       = 2
+
+  resource_id = "service/${aws_ecs_cluster.main.name}/${aws_ecs_service.app.name}"
+
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+}
+
+#########################################
+# Scale Out Policy (CPU > 70%)
+#########################################
+
+resource "aws_appautoscaling_policy" "scale_out" {
+  name               = "ecs-scale-out"
+  policy_type        = "TargetTrackingScaling"
+
+  resource_id        = aws_appautoscaling_target.ecs.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+
+    target_value       = 70
+    scale_in_cooldown  = 60
+    scale_out_cooldown = 60
+  }
+}
